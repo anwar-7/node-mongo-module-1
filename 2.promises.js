077346d475c871bf6d4da39d6202ea020a600fbe
anwarul1 <anwarjewel007@gmail.com>
2022-08-24 14:40:44 +0600

    promises

---
+++ 2.promises.js
@@ -0,0 +1,34 @@
+/* 
+const myPromise = new Promise((resolve, reject) => {
+  const user = null;
+
+  if (!user) {
+    reject('Something went wrong!');
+  } else {
+    setTimeout(() => {
+      resolve('Successfully get the data');
+    }, 1000);
+  }
+});
+
+myPromise
+  .then((res) => console.log('From Then:', res))
+  .catch((err) => console.log('From Catch:', err));
+ */
+/* 
+const myPromise2 = new Promise((resolve, reject) => {
+  const user = null;
+
+  if (!user) {
+    reject('Something went wrong!');
+  } else {
+    setTimeout(() => {
+      resolve('Successfully get the data...');
+    }, 1000);
+  }
+});
+
+myPromise2
+  .then((res) => console.log('From Then:', res))
+  .catch((err) => console.log('From Catch:', err));
+ */
